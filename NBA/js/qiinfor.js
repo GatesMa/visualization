b951660fa88e80ae6c6c7736e068c1fa711508d0
gatesma <gatesma@foxmail.com>
2020-05-12 10:27:19 +0800

    web

---
+++ NBA/js/qiinfor.js
@@ -0,0 +1,153 @@
+var qi11="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">凯里-欧文</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Kyrie Andrew Irving</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/kaili-ouwen.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#2</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 后卫 &nbsp 1.91m  &nbsp 88kg<br/>\n" +
+    "                                生日：1992年03月23日 &nbsp 选秀：2011年</p>\n" +
+    "                        </div>";
+var qi12="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">勒布朗-詹姆斯</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">LeBron James</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/zhanmusi.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#23</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 前锋-后卫 &nbsp 2.03m  113kg<br/>\n" +
+    "                                生日：1984年12月30日 &nbsp 选秀：2003年</p>\n" +
+    "                        </div>";
+var qi13="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">凯文-乐福</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Kevin Wesley Love</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/kaiwen-lefu.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#0</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 前锋 &nbsp 2.08m  114kg<br/>\n" +
+    "                                生日：1988年9月7日 &nbsp 选秀：2008年</p>\n" +
+    "                        </div>";
+
+var qi14="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">特里斯坦-汤普森</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Tristan Trevor James Thompson</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/telisitan-tangpusen.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#13</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 前锋 &nbsp 2.08m  108kg<br/>\n" +
+    "                               生日：1991年3月13日 &nbsp 选秀：2011年</p>\n" +
+    "                        </div>";
+
+var qi15="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">J.R.史密斯</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Earl Smith III</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/JR-simisi.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#5</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 后卫 &nbsp 1.98m  102kg<br/>\n" +
+    "                                生日：1985年9月9日 &nbsp 选秀：2004年</p>\n" +
+    "                        </div>";
+
+var qi16="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">伊曼-香珀特</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Iman Asante Shumpert</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/xiangbote.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#4</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 后卫 &nbsp 1.96m  100kg<br/>\n" +
+    "                                生日：1990年6月26日 &nbsp 选秀：2011年</p>\n" +
+    "                        </div>";
+
+var qi17="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">理查德-杰弗森</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Richard Allen Jefferson</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/jiefusen.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#24</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 前锋 &nbsp 2.01m  106kg<br/>\n" +
+    "                                生日：1980年6月21日 &nbsp 选秀：2001年</p>\n" +
+    "                        </div>";
+
+var qi18="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">钱宁-弗莱</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Channing Thomas Frye</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/qianning-fulai.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#9</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp前锋-中锋 &nbsp 2.11m  116kg<br/>\n" +
+    "                                生日：1983年5月17日 &nbsp 选秀：2005年</p>\n" +
+    "                        </div>";
+
+var qi19="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">马修-德拉维多瓦</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Matthew Dellavedova</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/maxiu.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#8</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp前锋-中锋 &nbsp 1.93m  91kg<br/>\n" +
+    "                                 生日：1990年9月8日 &nbsp 选秀：非选秀球员</p>\n" +
+    "                        </div>";
+
+var qi110="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">邓台-琼斯</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Dahntay Lavall Jones</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/dengtai-qiongsi.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#30</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 后卫 &nbsp 1.98m  95kg<br/>\n" +
+    "                                生日：1980年12月27日 &nbsp 选秀：2003年</p>\n" +
+    "                        </div>";
+
+var qi111="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">莫里斯-威廉姆斯</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Maurice Williams</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/weilianmusi.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#52</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 后卫 &nbsp 1.85m  90kg<br/>\n" +
+    "                                生日：1982年12月19日 &nbsp 选秀：2003年</p>\n" +
+    "                        </div>";
+
+var qi112="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">季莫费-莫兹戈夫</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">Timofey Mozgov</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/mozigefu.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#2</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 后卫 &nbsp 1.91m  &nbsp 88kg<br/>\n" +
+    "                                生日：1992年03月23日 &nbsp 选秀：2011年</p>\n" +
+    "                        </div>";
+
+var qi113="<div id=\"qiplayer\" style=\"font-size:3vh;padding-top:1vh;text-align: center;color:black;font-weight: bold\">詹姆斯-琼斯</div>\n" +
+    "                        <p id=\"qienplayer\" style=\"text-align: center;font-size: 2vh;margin-bottom:0;color: #666666\">James Andrew Jones</p>\n" +
+    "                        <div class=\"col-md-3\">\n" +
+    "                            <img id=\"tx\" src=\"img/photo/qi/qiongsi.png\" style=\"width:auto;height:9vh\">\n" +
+    "                        </div>\n" +
+    "                        <div class=\"col-md-9\" style=\"color: black;text-align: right\">\n" +
+    "                            <p id=\"qienplayer\" style=\"font-size: 3vh\"><b>#1</b></p>\n" +
+    "                            <p id=\"qienplayer\" style=\"text-align: right\">骑士 &nbsp 前锋 &nbsp 2.03m  99kg<br/>\n" +
+    "                                生日：1980年10月4日 &nbsp 选秀：2003年</p>\n" +
+    "                        </div>";
+var qisc1=[26, 3, 0, 4,3];
+var qisc2=[23, 2, 0, 9,12];
+var qisc3=[17, 1, 0, 2,13];
+var qisc4=[10, 0, 1, 0,12];
+var qisc5=[3, 0, 1, 1,1];
+var qisc6=[3, 0, 0, 0,1];
+var qisc7= [3, 0, 0, 0,2];
+var qisc8=[2, 0, 0, 0,1];
+var qisc9=[2, 0, 0, 1,0];
+var qisc10=[0, 0, 0, 0,0];
+var qisc11=[0, 1, 0, 0,0];
+var qisc12=[0, 0, 1, 0,2];
+var qisc13= [0, 0, 0, 0,0];
